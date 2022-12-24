0afbea8a3c502fa762cb0289194095f07cb7e603
NGUYEN DAT MINH <4601104100@student.hcmue.edu.vn>
2022-12-24 19:24:50 +0700

    add file

--- loadModel.py
+++ loadModel.py
@@ -19,9 +19,25 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import SparseCategoricalCrossentropy
 from tensorflow.keras.metrics import SparseCategoricalAccuracy
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+from tensorflow import keras
+import tensorflow as tf
+import numpy as np
+from tensorflow.compat.v1.keras import backend
 
 list_label = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
 label_map = {item: idx for idx, item in enumerate(list_label)}
+config = tf.compat.v1.ConfigProto(
+    device_count={'GPU': 1},
+    intra_op_parallelism_threads=1,
+    allow_soft_placement=True
+)
+
+config.gpu_options.allow_growth = True
+config.gpu_options.per_process_gpu_memory_fraction = 0.6
+
+session = tf.compat.v1.Session(config=config)
+
+backend.set_session(session)
 
 
 class DataLoader(Sequence):
@@ -92,7 +108,9 @@ class Classifier:
         self.model = model
 
     def load_model(self):
-        self.model = load_model('E:\AppEmotionInterface\models\emotion.h5')
+
+        self.model = load_model('models/emotionbest.h5')
+        self.model._make_predict_function()  # fix bug
 
     def save_model(self):
         pass
@@ -117,12 +135,17 @@ class Classifier:
         :param image: a PIL image with arbitrary size
         :return: a string ('Angry', 'Disgust','Fear','Happy','Neutral','Sad','Surprise')
         """
-        # image = image.resize([96, 96])
-        np_image = np.array(image)
-        batch_images = np.array([np_image])
-        y_prob_batch = self.model.predict(batch_images)
-        y_pred_batch = np.argmax(y_prob_batch, axis=1)
-        y_predict = y_pred_batch[0]
-        print("Kết quả mô hình dự đoán là:", self.list_labels[y_predict])
+        try:
+            with session.as_default():
+                with session.graph.as_default():
+                    # image = image.resize([96, 96])
+                    np_image = np.array(image)
+                    batch_images = np.array([np_image])
+                    y_prob_batch = self.model.predict(batch_images)
+                    y_pred_batch = np.argmax(y_prob_batch, axis=1)
+                    y_predict = y_pred_batch[0]
+                    # print("Kết quả mô hình dự đoán là:",self.list_labels[y_predict])
 
-        return self.list_labels[y_predict]
+                    return self.list_labels[y_predict]
+        except:
+            pass
--- main.py
+++ main.py
@@ -7,30 +7,44 @@ import facenet
 from PIL import Image
 import align.detect_face
 import tensorflow as tf
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.figure import Figure
+from PIL import Image
 ############################
+import sys
+
+
+matplotlib.use('Qt5Agg')
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.figure import Figure
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtGui import QPixmap, QColor
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt,QObject,QThread
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QObject, QThread
 from Display import Ui_MainWindow
 from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QVBoxLayout, QHBoxLayout, QFileDialog
+
 ###############################
-from tensorflow.keras.models import Model, load_model
 
-tf.compat.v1.disable_eager_execution()
-gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.6)
-sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
-with sess.as_default():
-    pnet, rnet, onet = align.detect_face.create_mtcnn(sess, "align")
-
-classifier = Classifier()
-classifier.load_model()
+plt.style.use('fivethirtyeight')
 
 INPUT_IMAGE_SIZE = 96
 MINSIZE = 20
 THRESHOLD = [0.6, 0.7, 0.7]
 FACTOR = 0.709
+
+tf.compat.v1.disable_eager_execution()
+gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.6)
+sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+with sess.as_default():
+    with sess.graph.as_default():
+        pnet, rnet, onet = align.detect_face.create_mtcnn(sess, "align")
+
+classifier = Classifier()
+classifier.load_model()
 
 
 class MainWindown(QMainWindow):
@@ -42,33 +56,65 @@ class MainWindown(QMainWindow):
         self.uic.ButtonStart.clicked.connect(self.start_capture_video)
         self.uic.ButtonStop.clicked.connect(self.stop_capture_video)
         self.thread = {}
+
     def closeEnvent(self, event):
         self.stop_capture_video()
+
     def start_capture_video(self):
         self.thread[1] = live_stream(index=1)
+
         self.thread[1].start()
-        self.thread[1].signal.connect(self.show_wedcam)
+        # self.time=time()
+        self.thread[1].signal1.connect(self.show_wedcam)
+        self.thread[1].signal2.connect(self.show_gh)
+
     def stop_capture_video(self):
         self.thread[1].stop()
+
     def show_wedcam(self, frame):
         qt_img = self.convert_cv_qt(frame)
+        qt_img_graph = self.convert_cv_qt_gh(frame)
         self.uic.label.setPixmap(qt_img)
+        self.uic.label_2.setPixmap(qt_img_graph)
+    def show_gh(self, frame):
+        qt_img_graph = self.convert_cv_qt_gh(frame)
+        self.uic.label_2.setPixmap(qt_img_graph)
+
     def convert_cv_qt(self, frame):
         rgb_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         h, w, ch = rgb_img.shape
-        bytes_per_line = ch*w
-        convert_to_Qt_format = QtGui.QImage(rgb_img.data, w,h,bytes_per_line, QtGui.QImage.Format_RGB888)
+        bytes_per_line = ch * w
+        convert_to_Qt_format = QtGui.QImage(rgb_img.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
         p = convert_to_Qt_format.scaled(800, 600, Qt.KeepAspectRatio)
         return QPixmap.fromImage(p)
+    def convert_cv_qt_gh(self, frame):
+        rgb_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        h, w, ch = rgb_img.shape
+        bytes_per_line = ch * w
+        convert_to_Qt_format = QtGui.QImage(rgb_img.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
+        p = convert_to_Qt_format.scaled(300, 300, Qt.KeepAspectRatio)
+        return QPixmap.fromImage(p)
+
+
 class live_stream(QThread):
-    signal = pyqtSignal(np.ndarray)
+    signal1 = pyqtSignal(np.ndarray)
+    signal2 = pyqtSignal(np.ndarray)
     def __init__(self, index):
         self.index = index
         print("start threading", self.index)
         super(live_stream, self).__init__()
+        self.countNegative = 0
+        self.countPositive = 0
+        self.time = 0
+
     def run(self):
+        self.time = time()
         self.run_programer()
-    def predict_model(self, frame):
+
+    def detect_face(self, frame):
+        # frame = cv2.flip(frame, 1)
+        det = None
+        bb = None
         bounding_boxes, _ = align.detect_face.detect_face(frame, MINSIZE, pnet, rnet, onet, THRESHOLD, FACTOR)
         faces_found = bounding_boxes.shape[0]
         try:
@@ -78,53 +124,110 @@ class live_stream(QThread):
             elif faces_found > 0:
                 det = bounding_boxes[:, 0:4]
                 bb = np.zeros((faces_found, 4), dtype=np.int32)
+
+        except:
+            pass
+        return bb, det, faces_found
+
+    def get_camera_stream(self):
+        return cv2.VideoCapture(0)
+    # def animationGraph(self, ):
+
+    def run_programer(self):
+        vd = self.get_camera_stream()
+        total_fps = 0
+        frame_count = 1
+        arrNegative = []
+        arrPositive = []
+        timeCount = []
+        while True:
+            start_time = time()
+            ret, frame = vd.read()
+            try:
+                bb, det, faces_found = self.detect_face(frame)
+
                 for i in range(faces_found):
                     bb[i][0] = det[i][0]
                     bb[i][1] = det[i][1]
                     bb[i][2] = det[i][2]
                     bb[i][3] = det[i][3]
-                    print(bb[i][3] - bb[i][1])
-                    print(frame.shape[0])
-                    print((bb[i][3] - bb[i][1]) / frame.shape[0])
+                    # print(bb[i][3]-bb[i][1])
+                    # print(frame.shape[0])
+                    # print((bb[i][3]-bb[i][1])/frame.shape[0])
                     if ((bb[i][3] - bb[i][1]) / frame.shape[0]) > 0.25:
-                        print("Emotion detected")
                         cropped = frame[bb[i][1]:bb[i][3], bb[i][0]:bb[i][2], :]
                         scaled = cv2.resize(cropped, (INPUT_IMAGE_SIZE, INPUT_IMAGE_SIZE),
                                             interpolation=cv2.INTER_CUBIC)
-                        cv2.imshow('Face ', scaled)
-                        # put rectangle to main image
-                        cv2.rectangle(frame, (bb[i][0], bb[i][1]), (bb[i][2], bb[i][3]), (0, 255, 0), 2)
-                        name = classifier.predict(scaled)
-                        # put name
-                        cv2.putText(frame, name, (bb[i][0], bb[i][1] - 10), cv2.FONT_HERSHEY_COMPLEX_SMALL,
-                                    1, (255, 255, 255), thickness=1, lineType=2)
-                        print("Name: {}".format(name))
+                        frame_count = frame_count + 1
+                        timeCount.append(frame_count)
 
-        except:
-            pass
-        return frame
-    def get_camera_stream(self):
-        return cv2.VideoCapture(0)
-    def run_programer(self):
-        vd = self.get_camera_stream()
-        assert vd.isOpened()
-        total_fps = 0
-        frame_count = 0
-        while True:
-            start_time = time()
-            ret, frame = vd.read()
-            assert ret
-            frame = self.predict_model(frame)
-            end_time = time()
-            fps = 1 / (end_time - start_time)
-            total_fps += fps
-            print(f"Frame Per Second: {round(fps, 1)}FPS")
-            self.signal.emit(frame)
+                        print(len(arrNegative))
+                        print(len(arrPositive))
+
+                        # convert scaled to image
+                        scaled = cv2.cvtColor(scaled, cv2.COLOR_BGR2RGB)
+                        name = classifier.predict(scaled)
+                        print(name)
+                        # self.signal.emit(frame)
+                        color = (255, 255, 255)
+                        if name == "Angry" or name == "Disgust" or name == "Fear" or name == "Sad":
+                            color = (0, 0, 255)
+                            self.countNegative += 1
+                            arrNegative.append(self.countNegative)
+                            arrPositive.append(self.countPositive)
+                        else:
+                            self.countPositive += 1
+                            arrPositive.append(self.countPositive)
+                            arrNegative.append(self.countNegative)
+                            color = (255, 0, 0)
+                        print(len(timeCount))
+                        # put name
+
+                        cv2.putText(frame, name, (bb[i][0], bb[i][1] - 10), cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                                    1, color, thickness=1, lineType=2)
+                        cv2.rectangle(frame, (bb[i][0], bb[i][1]), (bb[i][2], bb[i][3]), color, 2)
+                        fig = Figure(figsize=(5, 4), dpi=100)
+                        canvas = FigureCanvasAgg(fig)
+                        ax = fig.add_subplot(111)
+                        ax.plot(timeCount, arrPositive, color='red', label = 'x')
+                        ax.plot(timeCount, arrNegative, color='green', label ='y')
+                        canvas.draw()
+                        buf = canvas.buffer_rgba()
+                        img = Image.frombuffer('RGBA', canvas.get_width_height(), buf, 'raw', 'RGBA', 0, 1)
+                        X = np.asarray(buf)
+                        img = cv2.cvtColor(X, cv2.COLOR_RGBA2BGR)
+                end_time = time()
+                timerun = end_time - self.time
+                print('time', timerun)
+                if timerun > 120:
+                    if self.countNegative > self.countPositive:
+                        print("Negative")
+                        print("Negative", self.countNegative)
+                        print("Positive", self.countPositive)
+                    else:
+                        print("Positive")
+                        print("Negative", self.countNegative)
+                        print("Positive", self.countPositive)
+                    # self.countNegative = 0
+                    # self.countPositive = 0
+                    self.time = time()
+                fps = 1 / (end_time - start_time)
+                total_fps += fps
+                print(f"Frame Per Second: {round(fps, 1)}FPS")
+                self.signal1.emit(frame)
+                self.signal2.emit(img)
+            except:
+                pass
+
     def stop(self):
         print("stop threading", self.index)
         self.terminate()
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main_win = MainWindown()
     main_win.show()
     sys.exit(app.exec())
+
+
